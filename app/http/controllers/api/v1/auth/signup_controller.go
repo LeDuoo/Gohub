08492f15715c8a0905a3063072a52b664a0f4876
ayang <ayang@163.com>
2022-03-26 17:08:47 +0800

    验证参数

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -1,40 +1,50 @@
 package auth
 
-import(
-	"fmt"
+import (
 	v1 "Gohub/app/http/controllers/api/v1"
 	"Gohub/app/models/user"
+	"Gohub/app/requests"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 // SignupController 注册控制器
-type SignupController struct{
+type SignupController struct {
 	v1.BaseAPIController
 }
 
-func (sc *SignupController) IsPhoneExist(c *gin.Context){
-	// 请求对象
-    type PhoneExistRequest struct {
-        Phone string `json:"phone"`
-    }
-    request := PhoneExistRequest{}
+func (sc *SignupController) IsPhoneExist(c *gin.Context) {
+	// 初始化请求对象
+	request := requests.SignupPhoneExistRequest{}
+	
 
-    // 解析 JSON 请求
-    if err := c.ShouldBindJSON(&request); err != nil {
-        // 解析失败，返回 422 状态码和错误信息
-        c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-            "error": err.Error(),
+	// 解析 JSON 请求
+	if err := c.ShouldBindJSON(&request); err != nil {
+		// 解析失败，返回 422 状态码和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"error": err.Error(),
+		})
+		// 打印错误信息
+		fmt.Println(err.Error())
+		// 出错了，中断请求
+		return
+	}
+
+	// 表单验证
+	errs := requests.ValidateSignupPhoneExist(&request,c)
+
+	// errs 返回长度不为0为错误
+	if len(errs) > 0 {
+		//验证失败返回422错误码
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+            "errors": errs,
         })
-        // 打印错误信息
-        fmt.Println(err.Error())
-        // 出错了，中断请求
         return
-    }
-
-    //  检查数据库并返回响应
-    c.JSON(http.StatusOK, gin.H{
-        "exist": user.IsPhoneExist(request.Phone),
-    })
+	}
+	//  检查数据库并返回响应
+	c.JSON(http.StatusOK, gin.H{
+		"exist": user.IsPhoneExist(request.Phone),
+	})
 }
--- go.mod
+++ go.mod
@@ -35,6 +35,7 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/thedevsaddam/govalidator v1.9.10 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
 	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
