90a00b707d615e293b0a75802fde3bfbf3bffc93
ayang <ayang@163.com>
2022-04-24 16:08:24 +0800

    发送邮件验证码

--- pkg/verifycode/verifycode.go
+++ pkg/verifycode/verifycode.go
@@ -6,8 +6,10 @@ import (
 	"Gohub/pkg/config"
 	"Gohub/pkg/helpers"
 	"Gohub/pkg/logger"
+	"Gohub/pkg/mail"
 	"Gohub/pkg/redis"
 	"Gohub/pkg/sms"
+	"fmt"
 	"strings"
 	"sync"
 )
@@ -53,6 +55,31 @@ func (vc *VerifyCode) SendSMS (phone string) bool {
 		Data: map[string]string{"code" : code},
 	})
 	
+}// SendEmail 发送邮件验证码，调用示例：
+//         verifycode.NewVerifyCode().SendEmail(request.Email)
+func (vc *VerifyCode) SendEmail(email string) error {
+
+	//生成验证码
+	code := vc.generateVerifyCode(email)
+
+	// 方便本地和 API 自动测试
+	if !app.IsProduction() && strings.HasSuffix(email, config.GetString("verifycode.debug_email_suffix")) {
+		return nil
+	}
+
+	content := fmt.Sprintf("<h1>您的 Email 验证码是 %v </h1>", code)
+
+	mail.Newmailer().Send(mail.Email{
+		From: mail.From{
+            Address: config.GetString("mail.from.address"),
+            Name:    config.GetString("mail.from.name"),
+        },
+        To:      []string{email},
+        Subject: "Email 验证码",
+        HTML:    []byte(content),
+	})
+
+	return nil
 }
 
 // CheckAnswer 检查用户提交的验证码是否正确，key 可以是手机号或者 Email
