4613c469437f5d2d62ca6d0774c7955a17fcb200
LeDuoo <1286272540@qq.com>
2023-02-24 16:46:59 +0800

    缓存友情链接列表

--- app/models/link/link_util.go
+++ app/models/link/link_util.go
@@ -2,8 +2,11 @@ package link
 
 import (
 	"Gohub/pkg/app"
+	"Gohub/pkg/cache"
 	"Gohub/pkg/database"
+	"Gohub/pkg/helpers"
 	"Gohub/pkg/paginator"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -38,4 +41,23 @@ func Paginate(c *gin.Context, perPage int) (links []Link, paging paginator.Pagin
 		perPage,
 	)
 	return
+}
+
+func AllCached() (links []Link) {
+	//设置缓存
+	cacheKey := "links:all"
+	//设置过期时间
+	expireTime := 120 * time.Minute
+	//查询缓存
+	cache.GetObject(cacheKey, &links)
+
+	//缓存内无数据
+	if helpers.Empty(links) {
+		links = All()
+		if !helpers.Empty(links) {
+			//将查询数据存储到缓存
+			cache.Set(cacheKey, links, expireTime)
+		}
+	}
+	return links
 }
