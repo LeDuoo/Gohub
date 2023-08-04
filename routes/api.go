c0855dbc6cb1c303d8c792dc7ce0912e7be1cc20
LeDuoo <1286272540@qq.com>
2023-08-04 11:23:19 +0800

    API 域名和前缀

--- routes/api.go
+++ routes/api.go
@@ -4,14 +4,20 @@ import (
 	controllers "Gohub/app/http/controllers/api/v1"
 	"Gohub/app/http/controllers/api/v1/auth"
 	"Gohub/app/http/controllers/api/v1/middlewares"
+	"Gohub/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
 
 // RegisterAPIRoutes 注册路由方法
 func RegisterAPIRoutes(r *gin.Engine) {
-	// v1路由分组
-	v1 := r.Group("/v1")
+	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
+	var v1 *gin.RouterGroup
+	if len(config.Get("app.api_domain")) == 0 {
+		v1 = r.Group("/api/v1")
+	} else {
+		v1 = r.Group("/v1")
+	}
 
 	v1.Use(middlewares.LimitIP("200-H"))
 	{
